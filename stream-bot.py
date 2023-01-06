fa18a1d334fb183295e52abf5922e7c0617d4eae
fcothiago <f.t.s.goncalves@gmail.com>
2023-01-05 21:48:13 -0300

    Main file added

---
+++ stream-bot.py
@@ -0,0 +1,7 @@
+from scheduling.sched import sbot
+
+s = sbot([('alanzoka','twitch','firefox',None),('coreano','twitch','firefox',None),('cellbit','twitch','firefox',None)])
+s.run()
+
+while True:
+    pass
