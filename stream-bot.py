890396c791129fe27ba0be4beb554ba3b76354ac
fcothiago <f.t.s.goncalves@gmail.com>
2023-01-06 19:47:41 -0300

    Basic GUI begined

--- stream-bot.py
+++ stream-bot.py
@@ -1,7 +1,48 @@
+from tkinter import *
 from scheduling.sched import sbot
+import json
 
-s = sbot([('alanzoka','twitch','firefox',None),('coreano','twitch','firefox',None),('cellbit','twitch','firefox',None)])
-s.run()
+class GUI(Frame):
+    def __init__(self,master,data,bot):
+        Frame.__init__(self, master)
+        self.master = master
+        self.data = data
+        self.bot = bot
+        self.after(1000, self.update)
+    def update(self):
+        self.pack(fill=BOTH, expand=1)
 
-while True:
-    pass
+        self.container = Frame(self,borderwidth=1,relief="sunken")
+        self.container.place(relx=0.5, rely=0.05,relwidth=0.95,relheigh=0.8,anchor=N)
+
+        for task in self.bot.tasks.values():
+            item = Frame(self.container,borderwidth=1,relief="groove")
+            item.pack(fill=X)
+            Label(item,text=task.streamer.name).pack(side="left")
+            Label(item,borderwidth=0,text="Online" if task.streamer.streamer.onlive else "Offline",relief="flat") .place(relx=0.5,rely=0.5,anchor=CENTER)
+            Button(item, text="X").pack(side="right")
+            if task.flag:
+                Button(item, text="Stop").pack(side="right")
+            else:
+                Button(item, text="Start").pack(side="right")
+
+        exitButton = Button(self, text="Add")
+        exitButton.place(relx=0.5, rely=0.925,anchor=CENTER)
+
+
+with open("streamers.json","r+") as file:
+    #Loading Streamers
+    data = None
+    try:
+        data = json.load(file)["streamers"]
+    except:
+        data = {"streamers":[]}
+    print(data)
+    bot = sbot(data)
+    bot.start()
+    #Starting GUI
+    root = Tk()
+    app = GUI(root,data,bot)
+    root.wm_title("Stream-robot")
+    root.geometry("320x320")
+    root.mainloop()
